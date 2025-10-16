fdf4053de6dade620c7a9d22508bc6176d982a5a
xhonatankonomi <xhoni8962@gmail.com>
2025-10-16 16:50:04 +0200

    feat: Add initial script for train ticket price calculation
    This commit introduces the initial script to calculate the price of a train ticket based on distance and applies discounts for minors and over 65s. It defines the cost per kilometer, applicable discounts, and sets up a listener for the calculation button.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,7 +1,13 @@
+/*
+ Questo script calcola il prezzo di un biglietto del treno in base alla distanza e applica sconti per minori e over 65.
+ Definisce il costo al chilometro, gli sconti applicabili e aggiunge un listener al bottone di calcolo.
+*/
 const prezzoKm = 0.21;
 const scontoMinorenni = prezzoKm - (prezzoKm * 0.2);
 const sconto65 = prezzoKm - (prezzoKm * 0.4);
 const bottone = document.getElementById('calcoloBtn');
+
+
 bottone.addEventListener('click', function () {
-    
+
 })

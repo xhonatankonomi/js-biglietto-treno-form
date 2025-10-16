556311d83ad44395b0822006db9b52ea7a938c1d
xhonatankonomi <xhoni8962@gmail.com>
2025-10-16 16:47:38 +0200

    feat: Add event listener to calculate button
    This commit adds an event listener to the calculate button.
    It also corrects a typo in the calculation for the senior discount.

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,7 @@
 const prezzoKm = 0.21;
 const scontoMinorenni = prezzoKm - (prezzoKm * 0.2);
-const sconto65 = prezzoKm - (prezzoKm * 0.4)
+const sconto65 = prezzoKm - (prezzoKm * 0.4);
+const bottone = document.getElementById('calcoloBtn');
+bottone.addEventListener('click', function () {
+    
+})
